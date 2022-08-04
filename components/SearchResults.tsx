ad39ba22450c57e7175b4283d7dc61e5ac7c622f
danilo cucharro <danicucharro@outlook.com>
2022-08-04 10:22:48 -0300

    products apenas sendo renderizados caso não sejam iguais a renderização anterior

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -19,3 +19,21 @@ export function SearchResults({ results }: SearchResultsProps) {
         </div>
     );
 }
+
+/**
+ * RENDERIZAÇÃO DO REACT
+ * 
+ * 1. Criar uma nova versão do componente (acontece sempre que o componente pai autalizar um filho)
+ * 2. Comparar com a versão anterior
+ * 3. Se houverem alterações, vai atualizar o que alterou
+ */
+
+
+/**
+ * MEMO - Quando posso usar?
+ * 
+ * 1. Pure Functional Components 
+ * 2. Renders too often
+ * 3. Re-renders with same props
+ * 4. Medium to big size (quantidade de dados dentro de um component só)
+ */
