b3cb1356b7e0327e91231879659dc62f17600cc6
danilo cucharro <danicucharro@outlook.com>
2022-08-08 10:36:05 -0300

    reduzindo a quantidade amostra de conteudo da lista de produtos com o react-virtualized, a pagina fica muito mais leve para o navegador carrega-la

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,4 +1,5 @@
-import { useMemo } from "react";
+import { List, ListRowRenderer } from 'react-virtualized'
+
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
@@ -15,12 +16,31 @@ interface SearchResultsProps {
 //
 
 export function SearchResults({ totalPrice, results, onAddToWishList }: SearchResultsProps) {
+    const rowRenderer: ListRowRenderer = ({ index, key ,style }) => {
+        return(
+            <div key={key} style={style}>
+                <ProductItem 
+                    product={results[index]}
+                    onAddToWishList={onAddToWishList}
+                />
+            </div>
+        );
+    }
 
     return(
         <div>
             <h2>{totalPrice}</h2>
 
-            {results.map(product => {
+            <List
+                height={300}
+                rowHeight={30}
+                width={900}
+                overscanRowCount={5}
+                rowCount={results.length}
+                rowRenderer={rowRenderer}
+            />
+
+            {/*results.map(product => {
                 return(
                     // eslint-disable-next-line react/jsx-key
                     <ProductItem 
@@ -29,7 +49,7 @@ export function SearchResults({ totalPrice, results, onAddToWishList }: SearchRe
                       onAddToWishList={onAddToWishList}
                     />
                 );
-            })}
+            })*/}
         </div>
     );
 }
